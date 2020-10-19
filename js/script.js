8857a708ec46e0234a2efe3af8a599b4a88baa96
Alexander-Shebanov <legionqq996@gmail.com>
2020-10-19 13:41:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+const allSkills = {
+    ['html-skill']: 9,
+    ['css-skill']: 9,
+    ['scss-skill']: 8,
+    ['less-skill']: 8,
+    ['bt-skill']: 4,
+    ['js-skill']: 5,
+    ['react-skill']: 4,
+    ['git-skill']: 4
+};
+
+
+function createSkills() {
+    for (const [key, value] of Object.entries(allSkills)) {
+        const section = document.getElementById(key);
+
+        for (let i = 0; i < value; i++) {
+            const parallelogram = document.createElement('div');
+            parallelogram.classList.add('parallelogram-completed');
+            section.appendChild(parallelogram);
+        }
+
+        for (let i = value; i < 10; i++) {
+            const parallelogram = document.createElement('div');
+            parallelogram.classList.add('parallelogram')
+            section.appendChild(parallelogram);
+        }
+    }
+}
+
+createSkills();
